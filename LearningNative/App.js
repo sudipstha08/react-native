00221ba44c0ed53b1d4751ec2b488bfca9344c01
sudipstha08 <sudipstha088@gmail.com>
2022-04-11 16:20:51 +0545

    feat: images

--- LearningNative/App.js
+++ LearningNative/App.js
@@ -1,13 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableWithoutFeedback, TouchableOpacity, TouchableNativeFeedback } from 'react-native';
 
 export default function App() {
   console.log("App executed")
   return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <SafeAreaView style={styles.container} >
+      <Text numberOfLines={3}>React Native provides a number of built-in Core Components ready for you to use in your app. You can find them all in the left sidebar (or menu above, if you are on a narrow screen). If you're not sure where to get started, take a look at the following categories:
+      </Text>
+      {/* <Image source={require("./assets/icon.png")}/> */}
+      <TouchableWithoutFeedback onPress={() => console.log("Image")}>
+      <Image fadeDuration={1000} 
+      // blurRadius={10} 
+      source={{ width: 200,height: 300, uri: "https://picsum.photos/200/300?grayscale" }} />
+
+      </TouchableWithoutFeedback>
+      <TouchableOpacity onPress={() => console.log("Image")}>
+      <Image fadeDuration={1000} 
+      // blurRadius={10} 
+      source={{ width: 200,height: 300, uri: "https://picsum.photos/200/300?grayscale" }} />
+
+      </TouchableOpacity>
       <StatusBar style="auto" />
-    </View>
+      <TouchableNativeFeedback>
+        <View style={{ width: 200, height: 70 , backgroundColor: "dodgerblue"}}></View>
+      </TouchableNativeFeedback>
+    </SafeAreaView>
   );
 }
 
@@ -15,7 +32,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 learning react native
 
 1. npm start
+2. npx react-native run-android
+3. adb devices
+
+
+1. https://reactnative.dev/docs/running-on-device
