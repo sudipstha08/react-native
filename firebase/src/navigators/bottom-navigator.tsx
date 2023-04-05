9c9fef41302f460bb4bd89fdde969ac3df263c73
sudiptha08 <sudiptha08@gmail.com>
2023-04-05 22:58:36 +0545

    feat: add icons

--- firebase/src/navigators/bottom-navigator.tsx
+++ firebase/src/navigators/bottom-navigator.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import Icon from 'react-native-vector-icons/Ionicons';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import MaterialIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   HomeScreen,
   ProfileScreen,
@@ -18,11 +22,71 @@ export const BottomNavigator = () => {
       screenOptions={{
         headerShown: false,
       }}>
-      <BottomTabNav.Screen name="Home" component={HomeScreen} />
-      <BottomTabNav.Screen name="Profile" component={ProfileScreen} />
-      <BottomTabNav.Screen name="Message" component={MessageScreen} />
-      <BottomTabNav.Screen name="Notification" component={NotificationScreen} />
-      <BottomTabNav.Screen name="Bookmark" component={BookmarkScreen} />
+      <BottomTabNav.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Icon name="bank" size={30} />
+            ) : (
+              <Icon name="ios-person" size={30} />
+            );
+          },
+        }}
+      />
+      <BottomTabNav.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Icon name="bank" size={30} />
+            ) : (
+              <Icon name="user" size={30} />
+            );
+          },
+        }}
+      />
+      <BottomTabNav.Screen
+        name="Message"
+        component={MessageScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <MaterialIcon name="email-box" size={30} />
+            ) : (
+              <MaterialIcon name="email-box" size={30} />
+            );
+          },
+        }}
+      />
+      <BottomTabNav.Screen
+        name="Notification"
+        component={NotificationScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Icon name="bell" size={30} />
+            ) : (
+              <Icon name="bell" size={30} color="#999" />
+            );
+          },
+        }}
+      />
+      <BottomTabNav.Screen
+        name="Bookmark"
+        component={BookmarkScreen}
+        options={{
+          tabBarIcon: ({focused}) => {
+            return focused ? (
+              <Icon name="bookmark" size={30} />
+            ) : (
+              <Icon name="bookmark" size={30} />
+            );
+          },
+        }}
+      />
     </BottomTabNav.Navigator>
   );
 };
