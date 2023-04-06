01b9317004299bc2456a9cde7900e09a596a9ebf
sudiptha08 <sudiptha08@gmail.com>
2023-04-06 23:04:46 +0545

    feat: add firebase in ios

--- nativeweek/src/screens/index.ts
+++
@@ -1,6 +0,0 @@
-export {BookmarkScreen} from './Bookmarks';
-export {HomeScreen} from './Home';
-export {MessageScreen} from './Messages';
-export {NotificationScreen} from './Notifications';
-export {ProfileScreen} from './Profile';
-export {AccountScreen} from './Account';
